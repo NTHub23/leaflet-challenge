47e22471d4621a34190e726984ebe5e0eda66e25
NTHub23 <138403390+NTHub23@users.noreply.github.com>
2023-11-20 18:13:05 -0800

    Add files via upload

--- Leaflet-Part-2/static/js/logic.js
+++ Leaflet-Part-2/static/js/logic.js
@@ -1,8 +1,7 @@
-let basmap = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+
+let basmap = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png", {
     attribution : "Map data: &copy;"
 
-// let basmap = L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
-//     attribution : "Map data: &copy;"
 });
 
 let myMap = L.map("map", {
@@ -25,30 +24,23 @@ let overlays = {
 
 L.control.layers(BaseMaps, overlays).addTo(myMap)
 
+
 function getColor(depth) {
-    if(depth > 90){
-        return "#ea2c2c"
-    } else if (depth >70) {
-        return "#ea822c"
-    } else if (depth >50) {
-        return "#ee9c00"
-    } else if (depth > 30) {
-        return "#eecc00"
-    } else if (depth >10) {
-        return "#d4ee00"
+    if(depth > 90) return "#ea2c2c";
+        else if (depth >70)  return "#ea822c";
+        else if (depth >50)  return "#ee9c00";
+        else if (depth > 30) return "#eecc00";
+        else if (depth >10)  return "#d4ee00";
+    else return "#98ee00"
     }
-    else {
-        return "#98ee00"
-    }
-}
 
 function getRadius(magnitude) {
     if (magnitude === 0) {
         return 1
-    }
+    };
     return magnitude * 4
 
-}
+};
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
     console.log(data);
     function styleInfo(feature) {
@@ -62,7 +54,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
             weight:0.6
         }
     }
-
+    
     L.geoJson(data, {
         pointToLayer: function(feature, latlng) {
             return L.circleMarker(latlng);
@@ -94,8 +86,8 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     legend.addTo(myMap)
 
     d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function(plateData) {
-    L.geoJson(plateData, {
-        color : "red",
+    L.geoJSON(plateData, {
+        color : "orange",
         width : 3
     }).addTo(tectonicPlates);
 
